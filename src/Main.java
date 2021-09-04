991492722e75dc97d3e66665e2fcc9c132bc6a94
sunba <nsg2318@naver.com>
2021-09-04 11:21:25 +0900

    JavaStanFstCommit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int year = 0;
+        int age = 10;
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 1000;
+        age = year +1;
+        System.out.println(year);
+        System.out.println(age);
+
+    }
+}
