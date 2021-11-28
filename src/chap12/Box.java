8ae6527a87d81039c5c1b383c34fce9353506d68
sunba <nsg2318@naver.com>
2021-11-28 21:24:24 +0900

    interface 및 generic 공부

---
+++ src/chap12/Box.java
@@ -0,0 +1,13 @@
+package chap12;
+
+public class Box<T> {
+    T item;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+}
