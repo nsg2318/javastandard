5865e0b85f3b03fcce4695a3e7c6b7ae32e138fb
sunba <nsg2318@naver.com>
2021-12-22 10:50:00 +0900

    add : stack Example

---
+++ src/chap11/StackEx1.java
@@ -0,0 +1,34 @@
+package chap11;
+
+import java.util.Stack;
+
+public class StackEx1 {
+  public static Stack back = new Stack();
+  public static Stack forward = new Stack();
+
+  public static void main(String[] args) {
+    goURL("1.네이트");
+    goURL("2.야후");
+    goURL("3.네이버");
+    goURL("4.다음");
+    goURL("5.카카오");
+
+    printStatus();
+
+    
+  }
+
+  private static void printStatus() {
+    System.out.println("back:" + back);
+    System.out.println("forward" + forward);
+    System.out.println("현재 화면은 '" + back.peek()+ "' 입니다.");
+    System.out.println();
+  }
+
+  public static void goURL(String url) {
+    back.push(url);
+    if(!forward.empty())
+      forward.clear();
+  }
+
+}
