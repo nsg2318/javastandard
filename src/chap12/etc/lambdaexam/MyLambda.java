71298e97710fe62328b88d69f02199950c49e1c6
sunba <nsg2318@naver.com>
2021-12-27 21:26:01 +0900

    add lambda 유도 과정

--- src/chap12/etc/lambdaexam/MyLambda.java
+++ src/chap12/etc/lambdaexam/MyLambda.java
@@ -2,17 +2,10 @@ package chap12.etc.lambdaexam;
 
 public class MyLambda {
   public static void main(String[] args) {
-    System.out.println("메인 시작");
-    LambdaClass lambdaClass = new LambdaClass();
-    int c = lambdaClass.printMaxInt(10, 20);
+    Object f = new Object(){
+      int max(int a, int b) {
+        return a > b ? a : b;
+      }
+    };
   }
-}
-
-class LambdaClass{
-
-  int printMaxInt(int a, int b) {
-    return a > b ? a : b;
-  }
-
-
 }
