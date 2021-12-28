082ac681646a1e02dd5ecc9f360e7fef6021b44e
sunba <nsg2318@naver.com>
2021-12-28 16:25:40 +0900

    add : 컬렉션 프레임웍의 인터페이스 학습

--- src/chap14/LambdaEx4.java
+++ src/chap14/LambdaEx4.java
@@ -1,6 +1,10 @@
 package chap14;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.UnaryOperator;
 
 public class LambdaEx4 {
   public static void main(String[] args) {
@@ -9,7 +13,24 @@ public class LambdaEx4 {
       list.add(i);
     }
 
-    list.forEach(a -> System.out.println(a+","));
+    list.forEach(a -> System.out.print(a + ","));
+    System.out.println();
+    list.removeIf(x -> x % 2 == 0 || x % 3 == 0);
+    System.out.println(list);
+    list.replaceAll(ab -> ab * 10);
+    list.replaceAll(new UnaryOperator<Integer>() {
+      @Override
+      public Integer apply(Integer integer) {
+        return integer * 20;
+      }
+    });
+    System.out.println(list);
 
+    Map<String, String> map = new HashMap<>();
+    map.put("1", "1");
+    map.put("2", "2");
+    map.put("3", "3");
+    map.put("4", "4");
+    map.forEach((k, v) -> System.out.println(k + " " + v));
   }
 }
