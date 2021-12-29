d969a787a974c5d015fac347bd80240714715df5
sunba <nsg2318@naver.com>
2021-12-29 20:25:00 +0900

    add : Stream Exam 2 학습

--- src/chap14/StreamEx2.java
+++ src/chap14/StreamEx2.java
@@ -1,4 +1,20 @@
 package chap14;
 
+import java.io.File;
+import java.util.stream.Stream;
+
 public class StreamEx2 {
+  public static void main(String[] args) {
+    File[] fileArr = { new File("Ex1.java"), new File("Ex1.bak")
+        , new File("Ex2.java"), new File("Ex1"), new File("Ex1.txt")};
+
+    Stream<File> fileStream = Stream.of(fileArr);
+
+    //map()으로 Stream<File>을 Stream<String>으로 변환
+    Stream<String> stringStream = fileStream.map(File::getName);
+    stringStream.forEach(System.out::println);
+
+    fileStream = Stream.of(fileArr); //한 번 썼으니 다시 생성
+    fileStream.map(File::getName).peek(i -> System.out.println(i));
+  }
 }
