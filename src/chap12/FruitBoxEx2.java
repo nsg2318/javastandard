57264790aa52b0724f215747e4f29b84e45405e0
sunba <nsg2318@naver.com>
2021-12-30 08:46:55 +0900

    add : generics 학습1

---
+++ src/chap12/FruitBoxEx2.java
@@ -0,0 +1,37 @@
+package chap12;
+
+
+interface Eatable {
+}
+
+class FruitEx2 implements Eatable {
+  public String toString() {
+    return "Fruit";
+  }
+}
+
+class AppleEx2 extends FruitEx2 {
+  @Override
+  public String toString() {
+    return "Apple";
+  }
+}
+
+class GrapeEx2 extends FruitEx2 {
+  @Override
+  public String toString() {
+    return "Grape";
+  }
+}
+
+class ToyEx2 {
+  @Override
+  public String toString() {
+    return "Toy";
+  }
+}
+
+public class FruitBoxEx2 {
+  public static void main(String[] args) {
+  }
+}
