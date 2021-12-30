54b91d2bdebca7fd30b75e32ea96a5078748f220
sunba <nsg2318@naver.com>
2021-12-30 11:18:12 +0900

    add : ThreadEx1

---
+++ src/chap13/ThreadEx1.java
@@ -0,0 +1,31 @@
+package chap13;
+
+public class ThreadEx1 {
+  public static void main(String[] args) {
+    ThreadEx1_1 t1 = new ThreadEx1_1();
+    ThreadEx1_2 r = new ThreadEx1_2();
+    ThreadEx1_1 t3 = new ThreadEx1_1();
+    Thread t2 = new Thread(r);
+    t1.start();
+    t2.start();
+    t3.start();
+  }
+}
+
+class ThreadEx1_1 extends Thread {
+  @Override
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println(getName()); // Thread 의 getName
+    }
+  }
+}
+
+class ThreadEx1_2 implements Runnable {
+  @Override
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println(Thread.currentThread().getName());
+    }
+  }
+}
