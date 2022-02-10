1a16896305d01d8e8ae50345ea3af2a1d0febd5f
sunba <nsg2318@naver.com>
2022-02-10 20:06:51 +0900

    add : 거짓말 알고리즘 다시 풀기

--- src/algorithm/boj1043.java
+++ src/algorithm/boj1043.java
@@ -1,69 +1,39 @@
 package algorithm;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class boj1043 {
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
       //사람의 수
       int playerNum = scanner.nextInt();
+      List<Boolean> knownPeople = new ArrayList<>();
+      for (int i = 0; i < playerNum; i++) {
+        knownPeople.add(false);
+      }
+
       //파티의 수
       int partyNum = scanner.nextInt();
       //진실을 아는사람 수
       int known = scanner.nextInt();
       //진실을 아는사람 번호 리스트
-      ArrayList<Integer> knownId = new ArrayList<>(known);
+      Set<Integer> knownId = new HashSet<>(known);
+//      ArrayList<Integer> knownId = new ArrayList<>(known);
       for (int i=0; i < known; i++) {
         knownId.add(scanner.nextInt());
       }
-//        int zeroparty = 0;
-//        System.out.println(playerNum);
 
-      // 플레이어는 모두 false를 디폴트로 가지고 있음
-      ArrayList<Boolean> player = new ArrayList<>(playerNum);
-      for (int i=0; i < playerNum ; i++) {
-        player.add(false);
-//         System.out.println(player.get(i));
-      }
-      //그 중 진실을 아는 사람 번호리스트는 트루
-      for (int i=0; i < known; i++){
-        player.set(knownId.get(i)-1, true);
-      }
-//        System.out.println("파티시작 전 :"+player.toString());
-      ArrayList<ArrayList<Integer>> partyList = new ArrayList<>();
-
+      
       for (int i = 0; i < partyNum; i++) {
+        //해당 파티 참석 인원
         int partyPlayerNum = scanner.nextInt();
-//            if (partyPlayerNum == 0){
-//                zeroparty++;
-//            }
-
-        ArrayList<Integer> partyListList = new ArrayList<>();
+        List<Integer> partyPlayerIdList = new ArrayList<>();
         for (int j = 0; j < partyPlayerNum; j++) {
-          partyListList.add(scanner.nextInt());
+          partyPlayerIdList.add(scanner.nextInt());
         }
 
-        for(int j=0; j<partyListList.size(); j++) {
-          if(player.get(partyListList.get(j) - 1)){
-            for (Integer integer : partyListList) {
-              player.set(integer - 1, true);
-            }
-          }
-        }
-        partyList.add(partyListList);
       }
-      int cnt = 0;
-      for (ArrayList<Integer> integers : partyList) {
-        for (Integer integer : integers) {
-          if (player.get(integer - 1)) {
-            cnt++;
-            break;
-          }
-        }
-      }
-      System.out.println(partyList);
-      System.out.println("파티시작 한바퀴 후 :"+ player);
-      System.out.println(partyNum-cnt);
+
+
     }
   }
